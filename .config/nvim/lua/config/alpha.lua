e7eefd60318cd965d1c00db2e437e862cd6e3d3e
Asmit Das Choudhury <asmitdaschoudhury@proton.me>
2025-04-13 22:46:55 +0530

    UPD: Modified neovim statup dashbaord (added custom config for alpha-nvim).

---
+++ .config/nvim/lua/config/alpha.lua
@@ -0,0 +1,37 @@
+local status_ok, alpha = pcall(require, "alpha")
+if not status_ok then
+ return
+end
+
+local dashboard = require("alpha.themes.dashboard")
+dashboard.section.header.val = {
+
+ ' ███╗   ██╗ ███████╗ ██████╗  ██╗   ██╗ ██╗ ███╗   ███╗',
+ ' ████╗  ██║ ██╔════╝██╔═══██╗ ██║   ██║ ██║ ████╗ ████║',
+ ' ██╔██╗ ██║ █████╗  ██║   ██║ ██║   ██║ ██║ ██╔████╔██║',
+ ' ██║╚██╗██║ ██╔══╝  ██║   ██║ ╚██╗ ██╔╝ ██║ ██║╚██╔╝██║',
+ ' ██║ ╚████║ ███████╗╚██████╔╝  ╚████╔╝  ██║ ██║ ╚═╝ ██║',
+ ' ╚═╝  ╚═══╝ ╚══════╝ ╚═════╝    ╚═══╝   ╚═╝ ╚═╝     ╚═╝',
+}
+
+ dashboard.section.buttons.val = {
+   dashboard.button(":nf", "  New file", "<cmd>ene <CR>"),
+   dashboard.button(":ff", "󰈞  Find file", ":Neotree<CR>"),
+   dashboard.button(":to", "󰊄  Recently opened files"),
+   dashboard.button(":bm", "  Jump to bookmarks"),
+   dashboard.button(":tl", "  Open Telescope", ":Telescope<CR>"),
+   dashboard.button(":q", "  Bye Neovim", ":qa<CR>"),
+}
+
+local function footer()
+   return "I know you'll definitely bring her to life, so work harder..."
+end
+
+dashboard.section.footer.val = footer()
+
+dashboard.section.footer.opts.hl = "Type"
+dashboard.section.header.opts.hl = "Include"
+dashboard.section.buttons.opts.hl = "Keyword"
+
+dashboard.opts.opts.noautocmd = true
+alpha.setup(dashboard.opts)
