4927b4bf673427816bf2348cf0bb30c12f03a4bf
Asmit Das Choudhury <asmitdaschoudhury@proton.me>
2025-05-03 16:38:42 +0530

    INIT: Added and Modified few plugin configurations for neovim (lsp & alpha).

--- .config/nvim/lua/config/alpha.lua
+++ .config/nvim/lua/config/alpha.lua
@@ -1,30 +1,38 @@
 local status_ok, alpha = pcall(require, "alpha")
 if not status_ok then
- return
+    return
 end
 
 local dashboard = require("alpha.themes.dashboard")
 dashboard.section.header.val = {
 
- ' ███╗   ██╗ ███████╗ ██████╗  ██╗   ██╗ ██╗ ███╗   ███╗',
- ' ████╗  ██║ ██╔════╝██╔═══██╗ ██║   ██║ ██║ ████╗ ████║',
- ' ██╔██╗ ██║ █████╗  ██║   ██║ ██║   ██║ ██║ ██╔████╔██║',
- ' ██║╚██╗██║ ██╔══╝  ██║   ██║ ╚██╗ ██╔╝ ██║ ██║╚██╔╝██║',
- ' ██║ ╚████║ ███████╗╚██████╔╝  ╚████╔╝  ██║ ██║ ╚═╝ ██║',
- ' ╚═╝  ╚═══╝ ╚══════╝ ╚═════╝    ╚═══╝   ╚═╝ ╚═╝     ╚═╝',
+    -- [[██╗   ██╗██╗   ██╗██╗  ██╗██╗███╗   ██╗ ██████╗       ███████╗ █████╗ ███╗   ██╗]],
+    -- [[╚██╗ ██╔╝██║   ██║██║ ██╔╝██║████╗  ██║██╔═══██╗      ██╔════╝██╔══██╗████╗  ██║]],
+    -- [[ ╚████╔╝ ██║   ██║█████╔╝ ██║██╔██╗ ██║██║   ██║█████╗███████╗███████║██╔██╗ ██║]],
+    -- [[  ╚██╔╝  ██║   ██║██╔═██╗ ██║██║╚██╗██║██║   ██║╚════╝╚════██║██╔══██║██║╚██╗██║]],
+    -- [[   ██║   ╚██████╔╝██║  ██╗██║██║ ╚████║╚██████╔╝      ███████║██║  ██║██║ ╚████║]],
+    -- [[   ╚═╝    ╚═════╝ ╚═╝  ╚═╝╚═╝╚═╝  ╚═══╝ ╚═════╝       ╚══════╝╚═╝  ╚═╝╚═╝  ╚═══╝]],
+
+    [[  ███╗   ██╗███████╗ █████╗ ██████╗ ██╗     ]],
+    [[  ████╗  ██║██╔════╝██╔══██╗██╔══██╗██║     ]],
+    [[  ██╔██╗ ██║█████╗  ███████║██████╔╝██║     ]],
+    [[  ██║╚██╗██║██╔══╝  ██╔══██║██╔══██╗██║     ]],
+    [[  ██║ ╚████║███████╗██║  ██║██║  ██║███████╗]],
+    [[  ╚═╝  ╚═══╝╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝]],
+
 }
 
- dashboard.section.buttons.val = {
-   dashboard.button(":nf", "  New file", "<cmd>ene <CR>"),
-   dashboard.button(":ff", "󰈞  Find file", ":Neotree<CR>"),
-   dashboard.button(":to", "󰊄  Recently opened files"),
-   dashboard.button(":bm", "  Jump to bookmarks"),
-   dashboard.button(":tl", "  Open Telescope", ":Telescope<CR>"),
-   dashboard.button(":q", "  Bye Neovim", ":qa<CR>"),
+dashboard.section.buttons.val = {
+    dashboard.button(":nf", "  New file", "<cmd>ene <CR>"),
+    dashboard.button(":ff", "󰈞  Find file", ":Neotree<CR>"),
+    dashboard.button(":to", "󰊄  Recently opened files"),
+    dashboard.button(":bm", "  Jump to bookmarks"),
+    dashboard.button(":tl", "  Open Telescope", ":Telescope<CR>"),
+    dashboard.button(":q", "  Bye Neovim", ":qa<CR>"),
 }
 
 local function footer()
-   return "I know you'll definitely bring her to life, so work harder..."
+    return "\nI know you'll definitely bring her to life, so keep moving forward as always..."
 end
 
 dashboard.section.footer.val = footer()
--- .config/nvim/lua/config/lspconfig.lua
+++ .config/nvim/lua/config/lspconfig.lua
@@ -1,16 +1,17 @@
 local lspconfig = require('lspconfig')
 
-lspconfig.arduino_language_server.setup {}
-lspconfig.ast_grep.setup {
-    filetypes = { "c", "cpp", "rust", "go", "java", "python", "html", "css", "kotlin", "dart", "lua" },
-}
-lspconfig.bashls.setup {}
-lspconfig.biome.setup {}
-lspconfig.grammarly.setup {}
-lspconfig.hyprls.setup {}
-lspconfig.lemminx.setup {}
-lspconfig.rubocop.setup {}
-lspconfig.sqls.setup {}
-lspconfig.systemd_ls.setup {}
-lspconfig.taplo.setup {}
-lspconfig.yamlls.setup {}
+lspconfig.arduino_language_server.setup {} -- Arduino
+lspconfig.basedpyright.setup {}            -- Python
+lspconfig.bashls.setup {}                  -- Bash / ZSH
+lspconfig.biome.setup {}                   -- JSON / JS
+lspconfig.clangd.setup {}                  -- C / C++
+lspconfig.cssls.setup {}                   -- CSS
+lspconfig.dockerls.setup {}                -- Docker
+lspconfig.html.setup {}                    -- HTML
+lspconfig.hyprls.setup {}                  -- Hypr configs
+lspconfig.lua_ls.setup {}                  -- Lua
+lspconfig.markdown_oxide.setup {}          -- Markdown (Obsidian)
+lspconfig.systemd_ls.setup {}              -- Systemd
+lspconfig.textlsp.setup {}                 -- Text / LATEXT
+lspconfig.taplo.setup {}                   -- TOML
+lspconfig.yamlls.setup {}                  -- YAML
