53d5391d654e272cec022f4f2e06a055d8611d3b
Asmit Das Choudhury <asmitdaschoudhury@proton.me>
2025-05-14 17:02:11 +0530

    UPD: Sourced multiple config files of plugins for neovim at init.lua.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,4 +1,26 @@
+-- Default NeoVim configuration files
 require('options')
 require('keymaps')
 require('lazy-nvim')
 require('colorscheme')
+require('completion')
+require('autocmds')
+require('lsp-handlers')
+
+-- Plugin config files
+require('config.lualine')
+require('config.spectre')
+require('config.dressing')
+require('config.mason')
+-- require('config.null-ls')
+require('config.lspconfig')
+require('config.neo-tree')
+require('config.alpha')
+require('config.lspkind')
+
+-- Plugin single-line config (didnt created config file for a single-line)
+require("toggleterm").setup{}
+require("bufferline").setup{}
+-- require("pywal").setup{}
+
+
