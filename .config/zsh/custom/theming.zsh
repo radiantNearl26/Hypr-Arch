7437e58ed3a70be983a366cab73d6eda58f72b98
Asmit Das Choudhury <asmitdaschoudhury@proton.me>
2025-09-16 20:39:29 +0530

    UPD: Disabled universal terminal theme generation through pywal (instead enabled individual theme import like in alacritty).

--- .config/zsh/custom/theming.zsh
+++ .config/zsh/custom/theming.zsh
@@ -9,10 +9,10 @@ alias grep='grep --color=auto'
 # Import colorscheme from 'wal' asynchronously
 # &   # Run the process in the background.
 # ( ) # Hide shell job control messages.
-(cat ~/.cache/wal/sequences &)
+# (cat ~/.cache/wal/sequences &)
 
 # Alternative (blocks terminal for 0-3ms)
-cat ~/.cache/wal/sequences
+# cat ~/.cache/wal/sequences
 
 # To add support for TTYs this line can be optionally added. [Use default colors for tty, looks better!]
 source ~/.cache/wal/colors-tty.sh
